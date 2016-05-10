8aaa471d1c660892de4720935d475f053b8b6df4
defc0n1 <fabiankliem@hotmail.com>
2016-05-10 21:53:39 +0000

    params

--- app/controllers/shoppe/promos_controller.rb
+++ app/controllers/shoppe/promos_controller.rb
@@ -2,6 +2,14 @@ module Shoppe
   class PromosController < ApplicationController
     before_filter { @active_nav = :promos }
 
+    def index
+    
+    end
+
+
+    def safe_params
+      params[:promo].permit(:first_name, :last_name, :company, :email, :phone, :mobile)
+    end
 
   end
 end
